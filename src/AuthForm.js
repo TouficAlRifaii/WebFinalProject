fda3477dce54a663219f97b68bb54b7e6f33df6b
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-12 14:53:13 +0200

    Frontend: Inserting a button in the form

--- src/AuthForm.js
+++ src/AuthForm.js
@@ -3,12 +3,14 @@ function AuthForm ({ type }){
     
     return(
         <div>
+            <h1>{type === "SignUp" ? "Create Account" : "Welcome Back"}</h1>
             <form onSubmit={e => e.preventDefault()}>
             
                 {type === "SignUp" ? <Input type="text" content="name" /> : null }
                 <Input type="email" content="email" />
                 <Input type="password" content="password"/>
             </form>
+            <Button type={type} />
         </div>
     )
 }
