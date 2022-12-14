c0ef0f228cfb98e6b9bd8ae8aeea68691ed466eb
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-14 20:16:04 +0200

    Frontend: Creating the Login form

--- src/Login.js
+++ src/Login.js
@@ -18,7 +18,41 @@ const Login = () => {
   }, [email, password]);
 
   return (
-    <div></div>
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      ></p>
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          ref={emailRef}
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          required
+        />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          required
+        />
+        <button>Log In</button>
+        <p>
+             Don't have an account? <br />
+              <span>
+                {/* I'll put a router link here later */}
+                <a href="#">Sign Up</a>
+              </span>
+            </p>
+      </form>
+    </section>
   );
 };
 
