6c142c685f3126bc78ba9fbcd93bb16cc52f9aa0
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-14 18:19:25 +0200

    Frontend: Creating hooks for register form

--- src/Register.js
+++ src/Register.js
@@ -1,1 +1,35 @@
 import { useRef , useState , useEffect } from "react";
+import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
+const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+
+const Register = () =>{
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState("");
+    const [validName, setValidName] = useState(false);
+    const [userFocus , setUserFocus] = useState(false);
+    
+    const [password, setPassword] = useState("");
+    const [validPassword, setValidPasword] = useState(false);
+    const [passwordFocus , setPasswordFocus] = useState(false);
+
+    const [matchPassword, setMatchPassword] = useState("");
+    const [validMatchPassword, setValidMatchPasword] = useState(false);
+    const [matchFocus , setMatchFocus] = useState(false);
+
+    
+    return (
+        <div>
+
+        </div>
+    )
+
+
+
+}
+
+export default Register; 
