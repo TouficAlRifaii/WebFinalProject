fef8ac332d2bbc6f33bd4a81fbede06578c7fde5
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-14 18:32:01 +0200

    Frontend: Adding useEffects for Register.js

--- src/Register.js
+++ src/Register.js
@@ -1,35 +1,60 @@
-import { useRef , useState , useEffect } from "react";
-import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState, useEffect } from "react";
+import {
+  faCheck,
+  faTimes,
+  faInfoCircle,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const PASSWORD_REGEX =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
-const Register = () =>{
-    const userRef = useRef();
-    const errRef = useRef();
+const Register = () => {
+  const userRef = useRef();
+  const errRef = useRef();
 
-    const [user, setUser] = useState("");
-    const [validName, setValidName] = useState(false);
-    const [userFocus , setUserFocus] = useState(false);
-    
-    const [password, setPassword] = useState("");
-    const [validPassword, setValidPasword] = useState(false);
-    const [passwordFocus , setPasswordFocus] = useState(false);
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
 
-    const [matchPassword, setMatchPassword] = useState("");
-    const [validMatchPassword, setValidMatchPasword] = useState(false);
-    const [matchFocus , setMatchFocus] = useState(false);
+  const [password, setPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
 
-    
-    return (
-        <div>
+  const [matchPassword, setMatchPassword] = useState("");
+  const [validMatchPassword, setValidMatchPasword] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
 
-        </div>
-    )
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
 
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
 
-}
+  useEffect(() => {
+    const result = PASSWORD_REGEX.test(password);
+    setValidPassword(result);
+    console.log(result);
+    console.log(password);
+    const match = password === matchPassword;
+    setValidMatchPasword(match);
+  }, [password, matchPassword]);
 
-export default Register; 
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, password, matchPassword]);
+
+  
+  return <div></div>;
+};
+
+export default Register;
