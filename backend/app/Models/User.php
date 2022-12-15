7af115c69f6e000a03bd90f0729d799fb3022e9c
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-15 20:22:39 +0200

    Backend: Updating User model and .env

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -4,11 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -17,6 +18,14 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     protected $fillable = [
         'name',
         'email',
