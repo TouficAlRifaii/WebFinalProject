1b87c7094871de5d9349b0dcf1ff88af7f11c9b0
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-15 14:19:49 +0200

    Frontend: Finishing the navBar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,40 @@
 import { Link, BrowserRouter as Router } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 import "../styles/navbar.css";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [mobile, setMobile] = useState(false);
   return (
     <nav className="navbar">
-      <div className="navContainer">
-        <h3 className="logo">SSN</h3>
+      <h3 className="logo">SSN</h3>
 
-        <ul className="nav-links">
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link  to="/services">
-            <li>Services</li>
-          </Link>
-          <Link  to="/login">
-            <li>Login</li>
-          </Link>
-          <Link to="/signup">
-            <li>SignUp</li>
-          </Link>
-          <Link to="/account">
-            <li>Account</li>
-          </Link>
-        </ul>
-        <button className="mobile-menu">
-        <FontAwesomeIcon icon={faBars} />
-        </button>
-      </div>
+      <ul className={mobile ? "nav-links-mobile" : "nav-links"}>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/services">
+          <li>Services</li>
+        </Link>
+        <Link to="/login">
+          <li>Login</li>
+        </Link>
+        <Link to="/signup">
+          <li>SignUp</li>
+        </Link>
+        <Link to="/account">
+          <li>Account</li>
+        </Link>
+      </ul>
+      <button className="mobile-menu-icon" onClick={() => setMobile(!mobile)}>
+        {mobile ? (
+          <FontAwesomeIcon className="menu" icon={faXmark} />
+        ) : (
+          <FontAwesomeIcon className="menu" icon={faBars} />
+        )}
+      </button>
     </nav>
   );
 };
