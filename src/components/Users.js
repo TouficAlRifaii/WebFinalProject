70194f92a3eff2a169242e92a3ffb6a6d71c8c8b
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-16 21:03:08 +0200

    Frontend: Fixing the users list

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,54 +1,78 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "../api/axios";
 import useAuth from "../hooks/useAuth";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const USERS_URL = "/getUsers";
 
-const Users = async () => {
-  const { users, setUsers } = useState([]);
+const Users = () => {
+  const [users, setUsers] = useState();
+  const navigate = useNavigate();
+  const location = useLocation();
 
   const { auth } = useAuth();
   const token = auth.token;
   const authHeader = "Bearer " + token;
-  const config = {
-    headers: {
-      Authorization: authHeader,
-    },
-  };
-  try{
-    const response = await axios.get(USERS_URL, config);
-    if(response.data['status'] === "success"){
-        setUsers(response.data.users);
-    }
-  }catch(error){
-    console.log(error.response.data);
-  }
-  return(
-    <section>
-        <h1>Users List</h1>
-        {users?.length ? (
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Role</th>
-                    <th>Permission</th>
-                </tr>
-                {users.map((user)=> 
-                    <tr>
-                        <td>{user?.name}</td>
-                        <td>{user.email}</td>
-                        <td>{user.isAdmin ? "Admin" : "Normal User" }</td>
-                        <td>{user.isBlocked ? "Blocked" : "Allowed"}</td>
-                    </tr>
-                )}
-            </table>
-        ) : <p>No users Registered</p>
-    }
-    </section>
-  )
-  
 
+  //   getUsers();
+
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const config = {
+          headers: {
+            Authorization: authHeader,
+          },
+        };
+        const response = await axios.get(USERS_URL, config);
+        if (response.data["status"] === "success") {
+          isMounted && setUsers(response.data.users);
+          console.log(response.data);
+        }
+      } catch (err) {
+        console.error(err);
+        //navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
+
+    getUsers();
+
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+
+  return (
+    <section>
+      <h1>Users List</h1>
+      {users?.length ? (
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Permission</th>
+            </tr>
+            {users.map((user) => (
+              <tr key={user.id}>
+                <td>{user?.name}</td>
+                <td>{user.email}</td>
+                <td>{user.isAdmin ? "Admin" : "Normal User"}</td>
+                <td>{user.isBlocked ? "Blocked" : "Allowed"}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>No users Registered</p>
+      )}
+    </section>
+  );
 };
 
 export default Users;
