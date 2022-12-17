cd516ed08df272b90de1cba43f849ae5e1b90a51
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-17 19:02:15 +0200

    Frontend: Linking Home to backend

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+import { useEffect } from "react";
+import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import Feed from "./Feed";
 
 const Home = () => {
-    const articles = {};
+  const axiosPrivate = useAxiosPrivate();
+  const [articles, setArticles] = useState();
+
+  useEffect(() => {
+    const getArticles = async () => {
+      const response = await axiosPrivate.get("/getArticles");
+      console.log(response);
+      if (response.data["status"] === "success") {
+        setArticles(response.data["articles"]);
+      }
+    };
+    getArticles();
+  }, []);
   return (
     <main className="Home">
-      {articles.length ? (
+      {articles ? (
         <Feed articles={articles} />
       ) : (
         <p style={{ marginTop: "2rem" }}>No Articles to display.</p>
