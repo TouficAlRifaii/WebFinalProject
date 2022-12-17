3b310f14a23c63ef02e3f533438c737fa7ec48d5
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-17 21:53:59 +0200

    Frontend: Linking newArticle

--- src/components/NewArticle.js
+++ src/components/NewArticle.js
@@ -1,14 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "../api/axios";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import "../styles/newArticle.css";
 
-const NewArticle = ({ handleSubmit }) => {
-  const [title, setTitle] = useState();
-  const [content, setContent] = useState();
+const NewArticle = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const navigate = useNavigate();
+
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [categoryId, setCategoryId] = useState();
   const [categories, setCategories] = useState();
+
   const axiosPrivate = useAxiosPrivate();
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
 
   useEffect(() => {
     const getCategories = async () => {
@@ -27,12 +36,50 @@ const NewArticle = ({ handleSubmit }) => {
     getCategories();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("title", title);
+      formData.append("content", content);
+      formData.append("category_id", categoryId);
+      const response = await axiosPrivate.post("/createArticle", formData);
+      console.log(response.data);
+      if (response.data["status"] === "success") {
+        setSuccess(true);
+        setErrMsg(response.data["message"]);
+        setTimeout(() => {
+          navigate("/");
+        }, 2000);
+      }
+    } catch (error) {
+      setErrMsg(error.response.data["message"]);
+    }
+  };
+
   return (
     <div className="body">
       <div className="newArticlecontainer">
         <section className="NewArticle">
-          <h1>New Article</h1>
+          {success ? (
+            <p
+              ref={errRef}
+              className={errMsg ? "successmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+          ) : (
+            <p
+              ref={errRef}
+              className={errMsg ? "errmsg" : "offscreen"}
+              aria-live="assertive"
+            >
+              {errMsg}
+            </p>
+          )}
           <form className="newArticleForm" onSubmit={handleSubmit}>
+            <h1>New Article</h1>
             <label htmlFor="ArticleTitle">Title:</label>
             <input
               id="ArticleTitle"
@@ -49,7 +96,10 @@ const NewArticle = ({ handleSubmit }) => {
               onChange={(e) => setContent(e.target.value)}
             />
 
-            <select className="classic">
+            <select
+              onChange={(e) => setCategoryId(e.target.value)}
+              className="classic"
+            >
               <option value="">Select Category</option>
               {categories ? (
                 categories.map((category) => (
