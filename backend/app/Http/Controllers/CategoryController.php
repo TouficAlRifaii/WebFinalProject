ad3a4e20c0049926e71e8367dc36bc0d5d4ec453
TouficAlRifaii <toufic.alrifaii@lau.edu>
2022-12-17 13:50:21 +0200

    Backend: Adding Delete and update and routes for category

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Category;
 use Illuminate\Http\Request;
+use DB;
 use Illuminate\Support\Facades\Auth;
 use Validator;
 
@@ -23,8 +24,8 @@ class CategoryController extends Controller
         if (Auth::check()) {
             $user = Auth::user();
             if ($user->isAdmin) {
-                $validate = Validator::make($request->all, [
-                    "name" => 'required|string|20',
+                $validate = Validator::make($request->all(), [
+                    'name' => 'required|string|max:30',
                 ]);
                 if ($validate->fails()) {
                     return response()->json([
@@ -34,9 +35,9 @@ class CategoryController extends Controller
                 }
                 $category = new Category;
                 $category->name = $request->name;
-                if($category->save()){
+                if ($category->save()) {
                     return response()->json([
-                        "status"=> "success"
+                        "status" => "success",
                     ]);
                 }
 
@@ -44,4 +45,47 @@ class CategoryController extends Controller
         }
 
     }
+    public function updateCategory(Request $request)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user->isAdmin) {
+                $validate = Validator::make($request->all(), [
+                    'name' => 'required|string|max:30',
+                ]);
+                if ($validate->fails()) {
+                    return response()->json([
+                        "status" => "failed",
+                        "message" => "Bad input",
+                    ], 400);
+                }
+                $category = Category::find($request->id);
+                $category->name = $request->name;
+                if ($category->save()) {
+                    return response()->json([
+                        "status" => "success",
+                        "message" => "Category Updated Successfully",
+                    ], 200);
+                }
+            }
+        }
+
+    }
+    public function deleteCategory(Request $request)
+    {
+        if (Auth::check()) {
+            $user = Auth::user();
+            if ($user->isAdmin) {
+
+                DB::delete('delete from categories where id = ?', [$request->id]);
+
+                return response()->json([
+                    "status" => "success",
+                    "message" => "Category Updated Successfully",
+                ], 200);
+
+            }
+        }
+    }
 }
+;
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,23 +1,29 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use Illuminate\Support\Facades\Route;
+
 Route::group([
     'middleware' => ['api', 'cors'],
 ], function ($router) {
-Route::controller(AuthController::class)->group(function () {
-    Route::post('login', 'login');
-    Route::post('register', 'register');
-    Route::post('logout', 'logout');
-    Route::get('refresh', 'refresh');
-    Route::post('blockUser', 'blockUser');
-    Route::post('unblockUser', 'unblockUser');
-    Route::get('me', 'me');
-    Route::get('testGet', 'testGet');
-    Route::get('getUsers', 'getUsers');
-    Route::get('getBlockedUsers', 'getBlockedUsers');
-    
-    Route::post('editProfile', 'editProfile');
-});
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('login', 'login');
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+        Route::get('refresh', 'refresh');
+        Route::post('blockUser', 'blockUser');
+        Route::post('unblockUser', 'unblockUser');
+        Route::get('me', 'me');
+        Route::get('testGet', 'testGet');
+        Route::get('getUsers', 'getUsers');
+        Route::get('getBlockedUsers', 'getBlockedUsers');
+        Route::post('editProfile', 'editProfile');
+    });
+    Route::controller(CategoryController::class)->group(function () {
+        Route::get("getCategories" , "getCategories");
+        Route::post("createCategory" , "createCategory");
+        Route::post("updateCategory" , "updateCategory");
+        Route::post("deleteCategory", "deleteCategory");
+    });
 });
